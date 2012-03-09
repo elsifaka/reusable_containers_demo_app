c268f5145822474d4e220de718c851058e87008e
Ndrianiaina Mandrantosoa <tosoa@jojopil.com>
2012-03-09 18:18:20 +0300

    Auto creation des entrees

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,4 +1,10 @@
 class BlogPost < ActiveRecord::Base
   validates_presence_of :name, :title, :content
-  has_one :entry, as: :content
+  has_one :entry, as: :destination_content
+
+  after_create :associate_entry
+
+  def associate_entry
+    self.create_entry name: "#{self.class.model_name.human}: #{self.name}", container: Container.first, title: self.title, permalink: "blogpost-#{self.id}"
+  end
 end
